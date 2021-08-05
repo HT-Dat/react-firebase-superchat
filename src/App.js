0f47da3d77826fc62cbb27cf54cbaafbdb9417a3
hotiendat661 <hotiendat661@gmail.com>
2021-08-05 20:05:38 +0700

    initialize config for firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
+firebase.initializeApp({
+  apiKey: "AIzaSyA3_E6iLr8A4JFDjvcVSzASttnWLisa36w",
+  authDomain: "superchat-53299.firebaseapp.com",
+  projectId: "superchat-53299",
+  storageBucket: "superchat-53299.appspot.com",
+  messagingSenderId: "951331474775",
+  appId: "1:951331474775:web:d316dd40a329a5e2c54212",
+  measurementId: "G-VEZJEB6ZN4"
+})
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 function App() {
   return (
     <div className="App">
