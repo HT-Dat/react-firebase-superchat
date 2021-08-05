0277d6099d4261726be9350492112d67ec40a081
hotiendat661 <hotiendat661@gmail.com>
2021-08-06 03:09:31 +0700

    Signout button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { useAuthState} from 'react-firebase-hooks/auth';
-import {useCollectionData} from 'react-firebase-hooks/firestore';
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 firebase.initializeApp({
   apiKey: "AIzaSyA3_E6iLr8A4JFDjvcVSzASttnWLisa36w",
   authDomain: "superchat-53299.firebaseapp.com",
@@ -11,32 +11,54 @@ firebase.initializeApp({
   storageBucket: "superchat-53299.appspot.com",
   messagingSenderId: "951331474775",
   appId: "1:951331474775:web:d316dd40a329a5e2c54212",
-  measurementId: "G-VEZJEB6ZN4"
-})
+  measurementId: "G-VEZJEB6ZN4",
+  databaseURL: "https://superchat-53299.firebaseio.com",
+});
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 function App() {
   const [user] = useAuthState(auth);
-  const signInWithGoogle = () =>{
+  const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
-  }
-  function SignIn(){
+  };
+  function SignIn() {
     return (
-      <button type="button" onClick={signInWithGoogle}>Sign in with google</button>
-    )
+      <button type="button" onClick={signInWithGoogle}>
+        Sign in with google
+      </button>
+    );
   }
-  function ChatRoom(){
-
+  function SignOut() {
+    return (
+      auth.currentUser && (
+        <button type="button" onClick={() => auth.signOut()}>
+          Sign out
+        </button>
+      )
+    );
+  }
+  function ChatMessage(props) {
+    const { text, uid } = props.message;
+    return <p>{text}</p>;
+  }
+  function ChatRoom() {
+    const messagesRef = firestore.collection("messages");
+    const query = messagesRef.orderBy("createdAt").limit(25);
+    const [messages] = useCollectionData(query, { idField: "id" });
+    return (
+      <>
+        <SignOut />
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </>
+    );
   }
   return (
     <div className="App">
-      <header className="App-header">
-          
-      </header>
-      <section>
-        {user ? <ChatRoom/> : <SignIn/>}
-      </section>
+      <header className="App-header"></header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
